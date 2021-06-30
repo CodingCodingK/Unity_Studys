243bf910e123fe01ec92a6d3710cd5043b707f83
CodingCodingK <48432483+CodingCodingK@users.noreply.github.com>
2021-06-30 23:32:27 +0800

    20210630

---
+++ StudyCodes/SIKI_EDU/20210627-XLua_JoyFish/XLua_FishingJoy/Assets/Scripts/HotfixScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+using System.IO;
+using System.Text;
+
+public class HotfixScript : MonoBehaviour {
+
+	private LuaEnv luaenv;
+
+	// Use this for initialization
+	void Start () {
+		luaenv = new LuaEnv();
+        luaenv.AddLoader(MyLoader);
+        luaenv.DoString("require 'fish'");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    void OnDestory()
+    {
+        luaenv.Dispose();
+    }
+
+
+    byte[] MyLoader(ref string filepath)
+    {
+        string path =
+            @"D:\github\Unity\StudyCodes\SIKI_EDU\20210627-XLua_JoyFish\XLua_FishingJoy\Assets\PlayerGamePackage\" +
+            filepath + ".lua.txt";
+        return Encoding.UTF8.GetBytes(File.ReadAllText(path));
+    }
+
+    
+}
