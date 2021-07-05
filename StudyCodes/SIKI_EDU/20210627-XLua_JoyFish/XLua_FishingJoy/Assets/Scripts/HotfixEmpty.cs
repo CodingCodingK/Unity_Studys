bc1be959dd91bf26c9ed8e235628bab832e7f858
CodingCodingK <48432483+CodingCodingK@users.noreply.github.com>
2021-07-05 23:04:25 +0800

    20210705

---
+++ StudyCodes/SIKI_EDU/20210627-XLua_JoyFish/XLua_FishingJoy/Assets/Scripts/HotfixEmpty.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+[Hotfix]
+public class HotfixEmpty : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		
+	}
+
+	void BehaviourMethod()
+	{
+		
+	}
+	
+}
