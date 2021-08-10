e156096f5e2bd0ddab6621bb853da6d3857b4287
CodingCodingK <48432483+CodingCodingK@users.noreply.github.com>
2021-08-10 22:54:20 +0800

    0810

---
+++ StudyCodes/SIKI_EDU/20210802-DarkGod/DarkGod/Assets/Plugins/Editor/ScriptsInfoRecoder.cs
@@ -0,0 +1,23 @@
+﻿/************************************************************
+    文件: ScriptsInfoRecoder.cs
+	作者: YAN
+    邮箱: 2470939431@qq.com
+    日期: 2021/08/10 22:22
+	功能: 记录脚本信息
+*************************************************************/
+
+using System;
+using System.IO;
+
+public class ScriptsInfoRecoder : UnityEditor.AssetModificationProcessor {
+    private static void OnWillCreateAsset(string path) {
+        path = path.Replace(".meta", "");
+        if (path.EndsWith(".cs")) {
+            string str = File.ReadAllText(path);
+            str = str.Replace("#CreateAuthor#", Environment.UserName).Replace(
+                              "#CreateTime#", string.Concat(DateTime.Now.Year, "/", DateTime.Now.Month, "/",
+                                DateTime.Now.Day, " ", DateTime.Now.Hour, ":", DateTime.Now.Minute, ":", DateTime.Now.Second));
+            File.WriteAllText(path, str);
+        }
+    }
+}
