0e7402b084b9681c57eeb3e92981416970add52b
YAN JUNJIE <48432483+CodingCodingK@users.noreply.github.com>
2021-09-13 18:28:48 +0800

    0913

---
+++ StudyCodes/SIKI_EDU/20210802-DarkGod/DarkGod_DB/SqlFile/darkgod.sql
@@ -0,0 +1,38 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost_3306
+Source Server Version : 50617
+Source Host           : localhost:3306
+Source Database       : darkgod
+
+Target Server Type    : MYSQL
+Target Server Version : 50617
+File Encoding         : 65001
+
+Date: 2021-09-13 18:28:22
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for account
+-- ----------------------------
+DROP TABLE IF EXISTS `account`;
+CREATE TABLE `account` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `acct` varchar(255) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `level` int(11) NOT NULL,
+  `exp` int(11) NOT NULL,
+  `power` int(11) NOT NULL,
+  `coin` int(11) NOT NULL,
+  `diamond` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of account
+-- ----------------------------
+INSERT INTO `account` VALUES ('1', 'test1', 'test', '', '1', '0', '150', '5000', '500');
